e053284cd22ec8c3ae16d5d2e0a5a7f5e0711e3e
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2023-04-21 20:58:29 -0300

    first commit

---
+++ src/routes/Catalog/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+import Header from '../../components/Header';
+import SearchBar from '../../components/SearchBar';
+
+export default function Catalog() {
+    return (
+        <>
+            <Header />
+
+            <SearchBar/>
+        </>
+    );
+}
