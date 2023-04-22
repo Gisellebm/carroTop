26c70d6eebcd2439e1ff4a6f7cdae5c5168380c6
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2023-04-21 22:35:43 -0300

    search bar

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,10 +1,10 @@
-import './styles.css';
+import "./styles.css";
 
 export default function SearchBar() {
-    return (
-        <form className='ct-container'>
-            <input type="text" placeholder='Digite sua busca'/>
-            <button type='submit'>Buscar</button>
-        </form>
-    )
+  return (
+    <form className="ct-search-bar ct-container">
+      <input type="text" placeholder="Digite sua busca" />
+      <button type="submit">Buscar</button>
+    </form>
+  );
 }
--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,13 +1,24 @@
-import './styles.css';
-import Header from '../../components/Header';
-import SearchBar from '../../components/SearchBar';
+import "./styles.css";
+import Header from "../../components/Header";
+import SearchBar from "../../components/SearchBar";
 
 export default function Catalog() {
-    return (
-        <>
-            <Header />
-
-            <SearchBar/>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <main className="ct-catalog-main">
+        <section id="ct-catalog-section">
+          <div className="ct-search-bar-container">
+            <SearchBar />
+          </div>
+          <div className="ct-catalog-cards-container">
+            <div>Teste</div>
+            <div>Teste</div>
+            <div>Teste</div>
+            <div>Teste</div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
